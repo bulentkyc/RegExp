e2218f2e126ee53caee39340d76f488ed07d5979
bulentkyc <bulent.kayici@digitalcareerinstitute.org>
2019-04-08 09:55:22 +0200

    task is updated

--- Task/main.js
+++ Task/main.js
@@ -4,13 +4,22 @@
     There should be an input for write pattern,
     below this a button and
     below this 10 more input boxes to test pattern.
-    Right or left side of every input put any html element to
-    show the results
+    Right or left side of every input put any html 
+    element(p tag, div, etc) to show the results
 
 2)  Create a js for functinality.
     When click the button take pattern from first input and
     check all the other inputs and show reults.
 
-Extra: Create a textbox and put all tries inside of this with
+    Hint:   to compare input boxes you can use 
+            document.getElementByClassName 
+            with for loop
+
+    Ex:
+    let pattern = prompt('write your pattern wiyhout /');
+    const regExObj = RegExp(pattern)
+    alert(regExObj.test('111ab'));
+
+Extra: Create a textbox and put all tries inside of this with 
     some seperator characters.
 */
