e790a8680dafa9bc35eb06fd367069c309207aab
bulentkyc <bulent.kayici@digitalcareerinstitute.org>
2019-04-08 09:43:05 +0200

    Task is entered

---
+++ Task/main.js
@@ -0,0 +1,16 @@
+//------------------------Task-------------------------//
+/*
+1)  Design a page for regex exercises.
+    There should be an input for write pattern,
+    below this a button and
+    below this 10 more input boxes to test pattern.
+    Right or left side of every input put any html element to
+    show the results
+
+2)  Create a js for functinality.
+    When click the button take pattern from first input and
+    check all the other inputs and show reults.
+
+Extra: Create a textbox and put all tries inside of this with
+    some seperator characters.
+*/
